1986bec88e914f305ac85a727fc59efd5f48d7ea
Aravintha Prathi Kumar A G <aravinthaprathikumar.gopal@chainsys.com>
2022-07-25 12:09:02 +0530

    first commit

--- src/main/java/com/chainsys/batmobile/controller/MVCcontroller.java
+++ src/main/java/com/chainsys/batmobile/controller/MVCcontroller.java
@@ -1,5 +1,13 @@
 package com.chainsys.batmobile.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MVCcontroller {
+	@GetMapping("/index")
+    public String index() {
+        return "index";
+    }
 
 }
